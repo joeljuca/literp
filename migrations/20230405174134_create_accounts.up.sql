a975d5dca29a66ab039bbcc94f337d611901d4b3
Joel Jucá <joelwallis@gmail.com>
2023-04-08 11:23:20 -0300

    feat: create table accounts

---
+++ migrations/20230405174134_create_accounts.up.sql
@@ -0,0 +1,18 @@
+create table accounts (
+  id int primary key,
+  created_at text not null,
+  updated_at text not null,
+
+  is_active int not null default 1,
+  name text not null,
+  email text not null,
+  phone text,
+
+  -- json
+  profile text,
+  attributes text
+) STRICT;
+
+create index idx_acc_uniq_name on accounts (name);
+create unique index idx_acc_uniq_email on accounts (email collate nocase);
+create unique index idx_acc_uniq_phone on accounts (phone);
